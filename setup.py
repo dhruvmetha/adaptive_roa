05d38baf66df8fa0ca74fd0842e8142aa882f0af
sumanth-tangirala <tangiralasumanth@gmail.com>
2026-07-09 10:42:39 -0400

    build: add gpytorch dependency for partx GP classifier

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="adaptive_roa",
     packages=find_packages(),
+    install_requires=[
+        "gpytorch>=1.11",  # partx: variational GP classifier surrogate
+    ],
 )
